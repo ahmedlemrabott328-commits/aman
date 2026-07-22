82d948895d3d785d260549a015a49f5a84cd8d83
ahmedlemrabott328-commits <ahmedlemrabott328@gmail.com>
2026-07-22 12:32:04 +0000

    Delete aman-admin directory

--- aman-admin/src/components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import clsx from 'clsx';
-
-export function Card({ className, children }: { className?: string; children: React.ReactNode }) {
-  return (
-    <div className={clsx('rounded-xl border border-sand-deep/60 bg-white p-5 shadow-card', className)}>
-      {children}
-    </div>
-  );
-}
